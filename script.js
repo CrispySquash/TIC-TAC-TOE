afcccc0774d318e913d81a7566a6bc474211701a
CrispySquash <50679388+CrispySquash@users.noreply.github.com>
2021-06-25 15:15:35 -0500

    initial commit
    initial commit. have most of html done and bagan messing around with js

---
+++ script.js
@@ -0,0 +1,15 @@
+const gameBoard = (() => {
+    let boardArray = ['','','','','','','','','']
+
+    let square1 = document.getElementById('square-1');
+    let square2 = document.getElementById('square-2');
+    let square3 = document.getElementById('square-3');
+    let square4 = document.getElementById('square-4');
+    let square5 = document.getElementById('square-5');
+    let square6 = document.getElementById('square-6');
+    let square7 = document.getElementById('square-7');
+    let square8 = document.getElementById('square-8');
+    let square9 = document.getElementById('square-9');
+
+    square1.innerHTML = 'X';
+  })();
